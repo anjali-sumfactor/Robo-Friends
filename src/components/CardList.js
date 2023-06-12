3c2b6f6ea56842cc7ce751641329514bb6a7f1e4
anjali-sumfactor <anjali.sumfactor@gmail.com>
2023-06-12 18:12:36 +0530

    error boundary

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,9 +2,7 @@ import React from 'react';
 import Card from './Card';
 
 export default function CardList({ robots }) {
-    if(true){
-        throw new Error('NOOOOOOO');
-    }
+   
     return (
         <div>
             {
